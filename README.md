c2536c840f7166fcba66e9a0c9877a08d717d07a
hailinxiao <hailinxiao@tencent.com>
2019-09-30 20:31:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,72 @@
+# lower-component
+
+面对越来越复杂的前端系统，往往一个路由页面需要集成非常的多的功能，而我们的屏幕现实区域是有限的，这时将屏幕外的内容模块在首屏延迟渲染是一种非常高效的优化手段。    
+`lower-component`通过给组件划分等级来确定组件的渲染时机
+
+### 基本使用
+#### 1，默认提供两个level策略   
+
+* async——内部使用setTimeout做延迟渲染
+
+```
+<lower-component level="async">
+    <your-component />
+</lower-component>
+```
+* viewport——被降级的组件进入屏幕视窗渲染
+
+```
+<lower-component level="viewport">
+    <your-component />
+</lower-component>
+```
+#### 2，自定义策略
+**level赋值为custom，custom指定具体的策略实现。**   
+
+* 提供一个函数，并返回promise（promise状态改变时渲染）
+
+```
+// stage.vue
+<template>
+    <lower-component level="custom" custom="getList">
+        <your-component />
+    </lower-component>
+</template>
+<script>
+	export default {
+		methods: {
+			getList () {
+				return new Promise((resolve, reject) => {
+					setTimeout(() => {
+						resolve()
+					}, 1000)
+				})
+			}
+		}
+	}	
+</script>
+```
+* 提供一个Boolean值（Boolean为真时渲染） 
+
+```
+// stage.vue
+<template>
+    <lower-component level="custom" custom="isLoading">
+        <your-component />
+    </lower-component>
+</template>
+<script>
+	export default {
+		data () {
+			return {
+				isLoading: false
+			}
+		}
+		created () {
+			this.$http.get('/list').then(() => {
+				this.isLoading = true
+			})
+		}
+	}	
+</script>
+```
---
+++ index.js
@@ -0,0 +1,88 @@
+const STRATEGY_MAP = {
+    'async': function (callback) {
+        setTimeout(() => callback())
+    },
+    'viewport': function (callback) {
+        const windowHeight = window.innerHeight
+        this.timer = setInterval(() => {
+            const { top } = this.$refs['lowerComponent'].getBoundingClientRect()
+            if (windowHeight + 30 >= top) {
+                callback()
+                clearInterval(this.timer)
+            }
+            this.$once('hook:beforeDestory', () => {
+                clearInterval(this.timer)
+            })
+        }, 100)
+    }
+}
+export default {
+    name: 'lower-component',
+    props: {
+        level: {
+            type: String,
+            default: 'async'
+        },
+        custom: {
+            type: [Function, Boolean],
+            default: () => Promise.resolve()
+        }
+    },
+    data () {
+        return {
+            isRender: false
+        }
+    },
+    mounted() {
+        if (this.level === 'custom') {
+            if (typeof this.custom === 'function') {
+                const customPromise = this.custom()
+                if (process.env.NODE_ENV !== 'production') {
+                    if (typeof customPromise.then !== 'function') {
+                        throw new Error('当custom是函数类型时，custom必须返回一Promise')
+                    }
+                }
+                customPromise.then(() => {
+                    this.updateLevel()
+                })
+                return
+            }
+            const unwatch = this.$watch(() => this.custom, (newVal) => {
+                if (newVal) {
+                    this.updateLevel()
+                    this.$nextTick(() => {
+                        unwatch()
+                    })
+                }
+            }, {
+                immediate: true
+            })
+            return
+        }
+        if (process.env.NODE_ENV !== 'production') {
+            if (!STRATEGY_MAP.hasOwnProperty(this.level)) {
+                throw new Error('不支持的升级策略')
+            }
+        }
+        STRATEGY_MAP[this.level].call(this, this.updateLevel)
+    },
+    methods: {
+        updateLevel () {
+            this.isRender = true
+        }
+    },
+    render (h) {
+        if (this.isRender) {
+            if (this.$slots.default) {
+                return this.$slots.default[0]
+            }
+            return this._e()
+        }
+        if (this.level === 'viewport') {
+            return h('div', {
+                ref: 'lowerComponent'
+            })
+        }
+        return this._e()
+    }
+}
